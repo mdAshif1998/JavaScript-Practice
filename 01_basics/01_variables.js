4637ca8163b4b9194faafe0be48589526d9c0492
Md Ashif Ali <75850685+mdAshif1998@users.noreply.github.com>
2024-03-12 04:53:14 +0000

    Learn datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 145567
+let accountEmail = "code@google.com"
+var accountPassword = "124567"
+
+accountCity = "Hello"
+/*
+ 
+*/
+// console.log(accountEmail)
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+
+// alert("Hello") // Using node.js not browser
+
+
+console.log(3 + 3) // Code readability should be high
+
+console.log("Hello")
+console.log(typeof "Hello")
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+// Premitive Datatype
+// number -> 2^53
+// bigint -> 
+// string -> ""
+// boolean -> true/false
+// null (standalone value)
+// undefined ->
+// symbol -> unique
+
+// object
+
+
+
+
