b01f9ac78e7baaf6ef558ae44728c480918383b3
Md Ashif Ali <75850685+mdAshif1998@users.noreply.github.com>
2024-03-13 04:59:54 +0000

    Operations and Comparision

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = undefined
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// "44" -> 44
+// "44dbf" -> NaN
+// true -> 1 and flase -> 0
+// nan -> 0
+// undefined -> NaN
+
+// ============= Operations =============
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+console.log(null >= "Hello");
+
+// === strict check
+
+console.log("2" === 2);
