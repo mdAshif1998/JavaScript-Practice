9754f01378518215616d4c9905fac7515cb1484f
Md Ashif Ali <75850685+mdAshif1998@users.noreply.github.com>
2024-03-15 11:01:12 +0000

    Object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,30 @@
+// literal, constructor, singleton
+
+// object literals
+
+// Object.create
+const mySym = Symbol("Key")
+const jsUser = {
+    name: "Hello",
+    "full name": "Hello World",
+    [mySym]: "mykey1",
+    age: 20,
+    location: "Earth",
+    email: "hello@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+
+}
+// console.log(jsUser["email"]);
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
+
+jsUser.greeting = function(){
+    console.log("Hello Js User");
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello Js User ${this.name}`);
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
