55ea86ab9aa650b5cb14fec67b207c40b2df9159
Md Ashif Ali <75850685+mdAshif1998@users.noreply.github.com>
2024-04-02 05:20:24 +0000

    Add Function

--- 07_projects/01_investment_calculator.js
+++ 07_projects/01_investment_calculator.js
@@ -1,53 +1,33 @@
 const formServiceDecades = document.querySelector(".form-service-decades")
-const validationField = document.querySelector(".validation-result")
+const validationField = document.querySelector("#validation")
+const validationTextField = document.querySelector('.validation-result')
 const decadeWiseIncrementField = document.querySelector(".form-decade-wise-increment")
-const insideIncrementCard = decadeWiseIncrementField.querySelector("#decade-wise-increment")
+const insideIncrementCard = document.querySelector("#decade-wise-increment")
 const submitButton = document.querySelector('#subt')
 const decadeField = document.querySelector('#decadeField')
 let allInc = []
-let end = false
+let play = true
+let incFieldCreation = false
 
-formServiceDecades.addEventListener('submit', function(e){
-    e.preventDefault()
-    const decades = parseInt(decadeField.value)
-    
-    if(validateNumber(decades)){
-        validationField.innerHTML = ''
-        addIncrementFieldForUserInput(decades)
-        addDecadeIncSubmitButton()
-        e.stopPropagation()
-        const decadeInValues = insideIncrementCard.querySelectorAll('input')
-        const decadeIncSubmitButton = insideIncrementCard.querySelector('.decadeIncSubmit')
-        decadeIncSubmitButton.addEventListener('submit', function(e){
-            e.preventDefault()
-            for (let index = 0; index < decades; index++) {
-                const incValue = parseInt(decadeInValues[index].value)
-                if (validateNumber(incValue)){
-                    allInc.push(incValue)
-                }
-                else{
-                    end = true
-                    break
-                }
-                
-            }
-            console.log(allInc);
-            e.stopPropagation()
-            // if(end){
-            //     resetSecondSubmit()
-            // }
-            })
-    }
-    else{
-        console.log("Enter a valid decade")
-        end = true
-    }
-
-    // if(end){
-    //     resetFirstSubmit()
-    // }
-    
-})
+if(play){
+    formServiceDecades.addEventListener('submit', function(e){
+        e.preventDefault()
+        const decades = parseInt(decadeField.value)
+        if(validateNumber(decades)){
+            addIncrementFieldForUserInput(decades)
+            addDecadeIncSubmitButton()
+            incFieldCreation = true
+            incFieldOperation(incFieldCreation, decades)
+            submitButton.setAttribute('disabled', '')
+            decadeField.setAttribute('disabled', '')
+        }
+        else{
+            submitButton.setAttribute('disabled', '')
+            decadeField.setAttribute('disabled', '')
+            addNewPlayButton()
+        }  
+    })
+}
 
 
 function addIncrementFieldForUserInput(numberOfDecade){
@@ -75,24 +55,70 @@ function addDecadeIncSubmitButton(){
     button.setAttribute('class', 'decadeIncSubmit')
     button.style.cssText = "margin-left: 10px"
     insideIncrementCard.appendChild(button)
+
 }
 
 function validateNumber(num){
     if(isNaN(num) || num === ''){
-        validationField.innerHTML = `Please give a valid number`
+        validationTextField.innerHTML = `Please give a valid number`
         return false
     }
     else{
         return true
-    }
-    
+    }   
 }
 
-function resetFirstSubmit(){
-    decadeField.value = ''
-    formServiceDecades.setAttribute('disabled', '')
+function incFieldOperation(incFieldFlag, decades){
+    decadeWiseIncrementField.addEventListener('submit', function(e){
+        e.preventDefault()
+        if(incFieldFlag){
+            const decadeInValues = insideIncrementCard.querySelectorAll('input')
+            const decadeIncSubmitButton = insideIncrementCard.querySelector('.decadeIncSubmit')
+            for (let index = 0; index < decades; index++) {
+                const incValue = parseInt(decadeInValues[index].value)
+                if (validateNumber(incValue)){
+                    allInc.push(incValue)
+                }
+                else{
+                    decadeIncSubmitButton.setAttribute('disabled', '')
+                    addNewPlayButton()
+                    break
+                }
+                
+            }
+            console.log(allInc);
+        }
+    })
 }
 
-function resetSecondSubmit(){
-    insideIncrementCard.removeChild('input')
+function addNewPlayButton(){
+    const button = document.createElement('input')
+    button.setAttribute('type', `Submit`)
+    button.setAttribute('value', `Start New`)
+    button.setAttribute('id', `${submitButton.getAttribute('id')}`)
+    button.setAttribute('class', 'newTry')
+    button.style.cssText = "margin-left: 10px"
+    validationField.appendChild(button)
+    endGame()
+}
+
+function endGame(){
+    play = false
+    const newGameButton = document.querySelector('.newTry')
+    validationField.addEventListener('click', function(e){
+        play = true
+        allInc = []
+        decadeField.value = ''
+        submitButton.removeAttribute('disabled', '')
+        decadeField.removeAttribute('disabled', '')
+        if(incFieldCreation){
+            const incInputField = insideIncrementCard.querySelectorAll('input')
+                incInputField.forEach(function(field){
+                    field.remove()
+                })
+        }
+        validationTextField.innerHTML = ''
+        validationField.removeChild(newGameButton)
+
+    })
 }
